2fb779e821c767b45e5ba28c4e7816725d46b3cd
ajinkyagh <ajinkyagh@gmail.com>
2021-06-05 17:07:17 +0000

    Users table creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action:set_article,only:[:show,:edit,:update,:destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     
     
@@ -9,7 +11,7 @@ class ArticlesController < ApplicationController
     end    
     
     def index
-        @articles=Article.all
+        @articles=Article.paginate(page: params[ :page],per_page: 2)
     end 
     
     def new
@@ -18,6 +20,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article=Article.new(article_params)
+        @article.user =current_user
         if @article.save
             flash[:notice] = "Article was created successfully"
             redirect_to article_path(@article)
@@ -54,5 +57,12 @@ class ArticlesController < ApplicationController
     
     def article_params
         params.require(:article).permit(:title,:description)
-    end    
+    end
+    
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
+    end
 end    
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_045411) do
+ActiveRecord::Schema.define(version: 2021_06_05_043101) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_05_31_045411) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
